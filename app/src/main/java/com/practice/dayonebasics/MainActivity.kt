1f0c12a6ef1e8e55eb83e91e599102e5d3e1d788
jithukrishnaju <jithukju@tigmaminds.com>
2025-03-30 22:47:58 +0530

    Basic calculator project by using Spacer, Modifiers, Row, column, remember.

--- app/src/main/java/com/practice/dayonebasics/MainActivity.kt
+++ app/src/main/java/com/practice/dayonebasics/MainActivity.kt
@@ -1,47 +1,66 @@
 package com.practice.dayonebasics
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.practice.dayonebasics.ui.theme.DayOneBasicsTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            DayOneBasicsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            var num1 by remember {
+                mutableStateOf("0")
+            }
+            var num2 by remember {
+                mutableStateOf("0")
+            }
+            Column( modifier = Modifier
+                .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.statusBars)) {
+                TextField(num1, onValueChange = {
+                    num1 = it
+                })
+                TextField(num2, onValueChange = {
+                    num2 = it
+                })
+                Row {
+                    Button(onClick = {
+                        val sum = num1.toInt() + num2.toInt()
+                        Toast.makeText(applicationContext, "Sum = $sum", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Text("Add")
+                    }
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Button(onClick = {
+                        val sum = num1.toInt() - num2.toInt()
+                        Toast.makeText(applicationContext, "Difference = $sum", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Text("Subtract")
+                    }
                 }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DayOneBasicsTheme {
-        Greeting("Android")
     }
 }
